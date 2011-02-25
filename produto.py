546736629f1bd2beab1f7d48f1d5589342d87216
Raquel Crespo <raquelpcrespo@gmail.com>
2011-02-25 10:18:14 -0300

    classe produtos (aparelhos)

---
+++ produto.py
@@ -0,0 +1,6 @@
+class produto:
+    def __init__(self, codprod, marca, modelo, numserie):
+        self.codprod = codprod
+        self.marca = marca
+        self.modelo = modelo
+        self.numserie = numserie
