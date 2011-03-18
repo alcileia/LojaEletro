53214d4cd6be19eadf705f1256e8876eb75d5325
Raquel Crespo <raquelpcrespo@gmail.com>
2011-03-18 19:23:11 -0300

    classe venda

--- venda.py
+++ venda.py
@@ -1,4 +1,10 @@
 class venda:
-    def __init__(self, datavenda):
+    def __init__(self, datavenda, cliente):
         self.datavenda = datavenda
+        self.cliente = cliente
+        self.produtos = []
+
+    def adicionarProduto(self, produto):
+        self.produtos.append(produto)
+        
 
