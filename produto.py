3453270f1c3f5792ae0030809b6c997c990345fe
Raquel Crespo <raquelpcrespo@gmail.com>
2011-04-01 23:47:03 -0300

    classe produtos (aparelhos)

--- produto.py
+++ produto.py
@@ -1,5 +1,11 @@
+import io
 class produto:
-    todosProdutos = []
+    produtos = []
+    codprod = ""
+    marca = ""
+    modelo = ""
+    numserie = 0
+    quantidade = 0
     def __init__(self):
         self.codprod = 0
         self.marca = ""
@@ -7,28 +13,47 @@ class produto:
         self.numserie = 0
         self.quantidade = 0
 
-    def __init__(self, codprod, marca, modelo, numserie):
+    def __init__(self, codprod, marca, modelo, numserie, quantidade):
         self.codprod = codprod
         self.marca = marca
         self.modelo = modelo
         self.numserie = numserie
-        self.quantidade = 0
+        self.quantidade = quantidade
     
     @staticmethod
     def guardarprodutos(self):
-        produto.todosProdutos.append(produto)
+         produto.produtos.append(self)
+
+    def remover(self):
+        produto.produtos.remove(self)
+    
+    @staticmethod    
+    def alteraproduto(self):
+        for i in range (0, len(produto.produtos)):
+            if produto.produtos[i].codprod == self.codprod:
+                produto.produtos[i] = self
+               
         
     def addestoque (self, qtd):
         self.quantidade += qtd
+        produto.alteraproduto(self)
 
     def removeestoque (self, qtd):
         self.quantidade -= qtd
+        produto.alteraproduto(self)
 
     @staticmethod
-    def listadisponiveis(self):
-        self.todosDisponiveis = []
-        for i in range (0, len(todosProdutos)):
-            if produto.todosProdutos[i].quantidade > 0:
-                self.todosDisponiveis.append(todosProdutos [i].modelo)
-        print self.todosDisponiveis
-        return self.todosDisponiveis
+    def listadisponiveis():
+        todosDisponiveis = []
+        for i in range (0, len(produto.produtos)):
+            if produto.produtos[i].quantidade > 0:
+                p = produto(produto.produtos[i].codprod, produto.produtos[i].marca, produto.produtos[i].modelo, produto.produtos[i].numserie, produto.produtos[i].quantidade)
+                todosDisponiveis.append(p)
+        return todosDisponiveis
+
+    @staticmethod
+    def buscarProduto(codprod):
+        for i in range (0, len(produto.produtos)):
+            if produto.produtos[i].codprod == codprod:
+                p = produto(produto.produtos[i].codprod, produto.produtos[i].marca, produto.produtos[i].modelo, produto.produtos[i].numserie, produto.produtos[i].quantidade)
+                return p
