cf39345ee5bb351a6ef300f9f065533090b84f32
Raquel Crespo <raquelpcrespo@gmail.com>
2011-04-01 23:52:00 -0300

    classe venda

--- venda.py
+++ venda.py
@@ -1,10 +1,31 @@
+from datetime import date
 class venda:
-    def __init__(self, datavenda, cliente):
-        self.datavenda = datavenda
-        self.cliente = cliente
-        self.produtos = []
+  todasVendas = []
+  def __init__(self, codvenda, datavenda, cliente):
+    self.codVenda = codvenda
+    self.datavenda = datavenda
+    self.cliente = cliente
+    self.produtos = []
 
-    def adicionarProduto(self, produto):
-        self.produtos.append(produto)
-        
+  def adicionarProduto(self, produto):
+    self.produtos.append(produto)
 
+  @staticmethod
+  def guardarvenda(self):
+    venda.todasVendas.append(self)
+
+  @staticmethod
+  def buscarVenda(self, codVenda):
+    for i in range (0, len(venda.todasVendas)):
+      if venda.todasVendas[i].codVenda == codVenda:
+        return venda.todasVendas[i]
+  
+  def haGarantia(self):
+    data = date.today()
+    datav = date(int(self.datavenda[6:10]), int(self.datavenda[3:5]), int(self.datavenda[0:2]))
+    ano = datav.year+1
+    dataGarantia= date(ano, datav.month, datav.day)       
+    if data <= dataGarantia:
+      return True
+    else: 
+      return False
