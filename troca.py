5a21f28aadc4c3aa94abb06000c2f13d08690e3a
Raquel Crespo <raquelpcrespo@gmail.com>
2011-04-01 23:50:23 -0300

    classe troca

--- troca.py
+++ troca.py
@@ -1,5 +1,19 @@
 class troca:
-    def __init__(self, defeito, datatroca):
+    trocas = []
+    def __init__(self, defeito, datatroca, venda, codprod):
         self.defeito = defeito
         self.datatroca = datatroca
+        self.venda = venda
+        self.codprod = codprod
+    
+    def adicionarTroca(self):
+        if self.venda.haGarantia:
+            self.trocas.append(self)
+
+    def mostrarquemtrocou(self):
+        return self.venda.cliente.nome
+
+    @staticmethod    
+    def mostrarprodutosdefeituosos():
+        return troca.trocas
 
