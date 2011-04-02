3977536286d8c2ba3cbf9c4b914557baa3833cb2
Raquel Crespo <raquelpcrespo@gmail.com>
2011-04-01 23:51:20 -0300

    classe cliente

--- cliente.py
+++ cliente.py
@@ -1,6 +1,17 @@
 class cliente:
+    clientes = []
     def __init__(self, codcliente, nome, end):
         self.codcliente = codcliente
         self.nome = nome
         self.end = end
 
+    @staticmethod
+    def guardarclientes(self):
+         cliente.clientes.append(self)
+         
+    @staticmethod
+    def buscarCliente(self, codCliente):
+        for i in range (0, len(cliente.clientes)):
+            if cliente.clientes[i].codcliente == codCliente:
+                c = cliente(cliente.clientes[i].codcliente, cliente.clientes[i].nome, cliente.clientes[i].end)
+                return c            
